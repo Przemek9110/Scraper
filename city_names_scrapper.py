cf9c733b1bb2dc6687e4e0bd6c02c6392ab51909
przemek <przemyslaw.kwiecinski91@gmail.com>
2022-01-02 04:46:30 +0100

    add city_name_scrapper.py and iteration thru cities

---
+++ city_names_scrapper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from requests import get
+import pandas as pd
+from tqdm import tqdm
+print('Creating cities.csv file...')
+path = 'voivodeship.csv'
+data = pd.read_csv(path, index_col=0)
+city_name = []
+
+
+def fetch_page(URL):
+    page = get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+
+def parse_city_name(city_name):
+    return str(city_name.replace(' ', '-').replace('ą', 'a').replace('ć', 'c').replace('ę', 'e').replace('ł', 'l') \
+        .replace('ń', 'n').replace('ó', 'o').replace('ś', 's').replace('ż', 'z').replace('ź', 'z').replace('""', ''))
+
+
+for index, URL in tqdm(enumerate(data['links'])):
+    soup = fetch_page(URL)
+    for info in soup.findAll('span', class_='link'):
+        city_name.append(parse_city_name(info.findAllNext('a', class_='tdnone')[0].get_text().strip().lower()))
+    city_name.pop()
+
+file = pd.DataFrame(city_name)
+file.to_csv('cities.csv', index=False)
+
--- olx_scrapper.py
+++ olx_scrapper.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+
 from bs4 import BeautifulSoup
 from requests import get
 from tqdm import tqdm
 from sqlalchemy import create_engine
 import pandas as pd
+import city_names_scrapper
+import time
 
-engine = create_engine('mysql+pymysql://root:root@/scrapper', encoding='UTF-8', echo=True)
-URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/'
+engine = create_engine('mysql+pymysql://root:zddatapol17@/scrapper', encoding='UTF-8', echo=True)
+# URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/'
 dictionary = {}
 title = []
 price = []
@@ -17,6 +20,7 @@ area = []
 plot_area = []
 type_of_building = []
 link = []
+city_list = []
 
 
 def parse_price(price):
@@ -31,14 +35,17 @@ def setup(argv):
     pass  # TODO
 
 
-def site_pages_count():
-    page = get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    return int(soup.findAll('span', class_='item fleft')[-1].get_text())
+def site_pages_count(URL):
+    try:
+        page = get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return int(soup.findAll('span', class_='item fleft')[-1].get_text())
+    except IndexError:
+        print(f'', end='')
 
 
-def offer_link_finder(number):
-    page = get(f'{URL}?page={number}')
+def offer_link_finder(URL, number):
+    page = get(f'{URL}&page={number}')
     soup = BeautifulSoup(page.content, 'html.parser')
     link_list = []
     for offer in soup.findAll('div', class_='offer-wrapper'):
@@ -47,7 +54,7 @@ def offer_link_finder(number):
     return link_list
 
 
-def page_scrapper(soup):
+def page_scrapper(soup):  # TODO add city name from URL
     for info in soup.findAll('div', class_='css-1wws9er'):
         try:
             title.append(info.findNext('h1', class_="css-r9zjja-Text eu5v0x0").get_text().strip())
@@ -58,6 +65,7 @@ def page_scrapper(soup):
             level.append(info.findAllNext('p', class_='css-xl6fe0-Text eu5v0x0')[2].get_text().strip())
             area.append(info.findAllNext('p', class_='css-xl6fe0-Text eu5v0x0')[6].get_text().strip())
             type_of_building.append(info.findAllNext('p', class_='css-xl6fe0-Text eu5v0x0')[5].get_text().strip())
+            city_list.append(city)
         except ValueError as e:
             print(f'Value Error {e}')
 
@@ -65,7 +73,7 @@ def page_scrapper(soup):
 
     dictionary = {'offer_title': title, 'price': price, 'offer_type': offer_type,
                   'price_per_meter': price_per_meter, 'level': level, 'area': area,
-                  'offer_type_of_building': type_of_building}
+                  'offer_type_of_building': type_of_building, 'city_name': city_list}
 
     return dictionary
 
@@ -74,21 +82,32 @@ def offer_iterator(link_list):
     for i in range(len(link_list)):
         site = get(link_list[i]['href'])
         soup = BeautifulSoup(site.content, 'html.parser')
-        a = page_scrapper(soup)
-    return a
+        result = page_scrapper(soup)
+    return result
 
 
-def main():
+def main(URL):
     print(f'\nApplication starts scraping from {URL}')
-    for offers_site in tqdm(range(site_pages_count())):
-        offer_link = offer_link_finder(offers_site)
+    for offers_site in tqdm(range(site_pages_count(URL))):
+        offer_link = offer_link_finder(URL, offers_site)
         diction = offer_iterator(offer_link)
     df = pd.DataFrame(diction)
-    df.to_sql('Offers', engine, if_exists='replace', index=False)
-    df['offer_title'].unique()
-    df.to_csv('title.csv', index=False)
-    print('WORK DONE!!')
+    df.to_sql('Offers', engine, if_exists='append', index=False)
+    df.to_csv('apartments.csv', mode='a', index=False)  # TODO Choice saving to DB or .csv
+    print('Scrapped!!')
 
 
 if __name__ == '__main__':
-    main()
+    print('Scrapper Runing...')
+    start_time = time.time()
+    data = pd.read_csv('cities.csv')
+    print('WORK START!!!')
+    for ind, city in enumerate(data['0']):
+        URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/' + city + '/?search%5Bfilter_enum_market%5D%5B0%5D=' \
+                    'secondary&search%5Bfilter_enum_rooms%5D%5B0%5D=two'
+        if site_pages_count(URL) is None: # TODO if is only one page don't wanna scrape
+            print(f'No offers: {URL}')
+        else:
+            main(URL)
+    print('WORK DONE!')
+    print('-----%s seconds-----', (time.time() - start_time))
