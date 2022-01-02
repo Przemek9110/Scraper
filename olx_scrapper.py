5c1b85f4da941e7a8dcdb8420f4d9b548ab7c81b
przemek <przemyslaw.kwiecinski91@gmail.com>
2022-01-02 21:56:19 +0100

    added handling exeptions for offers on one page site/ changes in main and offer_iterator functions

--- olx_scrapper.py
+++ olx_scrapper.py
@@ -5,11 +5,9 @@ from requests import get
 from tqdm import tqdm
 from sqlalchemy import create_engine
 import pandas as pd
-import city_names_scrapper
 import time
 
-engine = create_engine('mysql+pymysql://root:zddatapol17@/scrapper', encoding='UTF-8', echo=True)
-# URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/'
+engine = create_engine('mysql+pymysql://root:zddatapol17@/scrapper', encoding='UTF-8', echo=False)
 dictionary = {}
 title = []
 price = []
@@ -17,9 +15,7 @@ offer_type = []
 price_per_meter = []
 level = []
 area = []
-plot_area = []
 type_of_building = []
-link = []
 city_list = []
 
 
@@ -35,18 +31,26 @@ def setup(argv):
     pass  # TODO
 
 
+def fetch_page(URL):
+    page = get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+
 def site_pages_count(URL):
-    try:
-        page = get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        return int(soup.findAll('span', class_='item fleft')[-1].get_text())
-    except IndexError:
-        print(f'', end='')
+    soup = fetch_page(URL)
+    result = soup.findAll('span', class_='item fleft')
+    if len(result) > 0:
+        result = int(result[-1].get_text())
+    elif len(soup.findAll('h1', class_='c41 lheight24')) > 0:
+        return None
+    else:
+        result = 1
+    return result
 
 
 def offer_link_finder(URL, number):
-    page = get(f'{URL}&page={number}')
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = fetch_page(f'{URL}&page={number}')
     link_list = []
     for offer in soup.findAll('div', class_='offer-wrapper'):
         link = offer.findNext('a')
@@ -69,8 +73,6 @@ def page_scrapper(soup):  # TODO add city name from URL
         except ValueError as e:
             print(f'Value Error {e}')
 
-    # print(title, price, offer_type, price_per_meter, level, area, type_of_building)
-
     dictionary = {'offer_title': title, 'price': price, 'offer_type': offer_type,
                   'price_per_meter': price_per_meter, 'level': level, 'area': area,
                   'offer_type_of_building': type_of_building, 'city_name': city_list}
@@ -79,11 +81,13 @@ def page_scrapper(soup):  # TODO add city name from URL
 
 
 def offer_iterator(link_list):
-    for i in range(len(link_list)):
-        site = get(link_list[i]['href'])
-        soup = BeautifulSoup(site.content, 'html.parser')
-        result = page_scrapper(soup)
-    return result
+    try:
+        for i in range(len(link_list)):
+            soup = fetch_page(link_list[i]['href'])
+            result = page_scrapper(soup)
+        return result
+    except UnboundLocalError:
+        print(f'Page not found {URL}')
 
 
 def main(URL):
@@ -91,23 +95,26 @@ def main(URL):
     for offers_site in tqdm(range(site_pages_count(URL))):
         offer_link = offer_link_finder(URL, offers_site)
         diction = offer_iterator(offer_link)
-    df = pd.DataFrame(diction)
-    df.to_sql('Offers', engine, if_exists='append', index=False)
-    df.to_csv('apartments.csv', mode='a', index=False)  # TODO Choice saving to DB or .csv
-    print('Scrapped!!')
+    print("Scrapped!!\n")
+    return diction
 
 
 if __name__ == '__main__':
     print('Scrapper Runing...')
+
     start_time = time.time()
     data = pd.read_csv('cities.csv')
     print('WORK START!!!')
     for ind, city in enumerate(data['0']):
         URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/' + city + '/?search%5Bfilter_enum_market%5D%5B0%5D=' \
-                    'secondary&search%5Bfilter_enum_rooms%5D%5B0%5D=two'
-        if site_pages_count(URL) is None: # TODO if is only one page don't wanna scrape
+                                                                               'secondary&search%5Bfilter_enum_rooms%5D%5B0%5D=two'
+        if site_pages_count(URL) is None:
             print(f'No offers: {URL}')
         else:
-            main(URL)
+            df = pd.DataFrame(main(URL))
+
+    df.to_sql('Offers', engine, if_exists='append', index=False)  # TODO Saving wrong cities .csv is ok
+    df.to_csv('apartments.csv', mode='a', index=False)  # TODO Choice saving to DB or .csv
+
     print('WORK DONE!')
-    print('-----%s seconds-----', (time.time() - start_time))
+    print('-----%s seconds-----' % (time.time() - start_time))
